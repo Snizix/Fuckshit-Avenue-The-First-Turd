d64096c817d3b3aa2ff1de71d3b466baa321b6f3
rdm144 <46978172+rdm144@users.noreply.github.com>
2023-12-12 20:18:03 -0600

    Placed player Animator on player container

---
+++ Assets/C#_Scripts/Camera/CameraCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    Vector3 dollyDir;
+    float smooth = 10;
+    float distance;
+    float minDistance = 1;
+    float maxDistance = 4;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        dollyDir = transform.position.normalized;
+        distance = transform.position.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = dollyDir * maxDistance;
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.position, desiredPos, out hit))
+        {
+            distance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+        }
+        else
+            distance = maxDistance;
+        transform.position = Vector3.Lerp(transform.position, dollyDir * distance, Time.deltaTime * smooth);
+    }
+}
